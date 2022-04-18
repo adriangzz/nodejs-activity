6a80313f68bd5b92edc982efd0414407499145d1
adriangzz <adriangonzalezmontemayor@gmail.com>
2022-04-18 16:46:01 -0500

    Add bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+var path = require("path");
 var categoriesRouter = require("./category");
+
 const app = express();
 const port = 3000;
 
 app.use("/", categoriesRouter);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- category.js
+++ category.js
@@ -4,7 +4,7 @@ var db = require("./script");
 // another routes also appear here
 // this script to fetch data from MySQL databse table
 router.get("/", function (req, res, next) {
-  var sql = "SELECT * FROM users";
+  var sql = "SELECT * FROM category";
   db.query(sql, function (err, data, fields) {
     if (err) throw err;
     res.render("categories", { title: "User List", userData: data });
